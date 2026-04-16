2574bb8a427de9bdc59a5aeff58b7ada1c176a93
Richard Chibuike <franklinrichard2007@gmail.com>
2026-04-16 21:02:07 +0100

    First attempt at a controller

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Body, Put, Delete } from '@nestjs/common';
 
 @Controller('episodes')
 export class EpisodesController {
     @Get()
-    findAll() {
+    findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
+        console.log(sort)
         return "all episodes"
     }
 
+    @Get(":id")
+    findOne(@Param() id: string){
+        console.log(id);
+        return "one episode";
+    }
+
     @Post()
-    create(){
+    create(@Body() input: any){
+        console.log(input);
         return "new episodes"
+    }
+
+    @Get("featured")
+    findFeatured(){
+        return "featured episodes"
+    }
+
+    @Put(":id")
+    update(@Body() input: any, @Param() id: string){
+        console.log(input);
+        return "updated episode"
+    }
+
+    @Delete(":id")
+    delete(@Param() id: string){
+        return "deleted episode";
     }
 }
